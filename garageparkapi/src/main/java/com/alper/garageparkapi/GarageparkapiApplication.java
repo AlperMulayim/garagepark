294a17346a8a9836b42cf3bac77e473e08aacb7b
alpermulayim <alper.mulayim48@gmail.com>
2022-12-08 13:27:03 +0300

    Garage Park API init

---
+++ garageparkapi/src/main/java/com/alper/garageparkapi/GarageparkapiApplication.java
@@ -0,0 +1,13 @@
+package com.alper.garageparkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageparkapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageparkapiApplication.class, args);
+	}
+
+}
---
+++ garageparkapi/src/test/java/com/alper/garageparkapi/GarageparkapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alper.garageparkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageparkapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
