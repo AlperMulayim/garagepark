01c5788c71708d6023088411a194ce8c5de93281
alpermulayim <alper.mulayim48@gmail.com>
2022-12-10 16:42:09 +0300

    GarageParkLogger aspect added for methods.

---
+++ garageparkapi/src/main/java/com/alper/garageparkapi/logger/GarageParkLogger.java
@@ -0,0 +1,35 @@
+package com.alper.garageparkapi.logger;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+public class GarageParkLogger {
+    private static  final String POINT_CUT = "within(com.alper.garageparkapi..*)";
+
+    @Around(POINT_CUT)
+    public Object logExec(ProceedingJoinPoint proceedingJoinPoint) {
+        Object proceed = null;
+
+        try {
+            Signature signature = proceedingJoinPoint.getSignature();
+            MethodSignature methodSignature = (MethodSignature) signature;
+            Method method = methodSignature.getMethod();
+            proceed =  proceedingJoinPoint.proceed();
+            System.out.println("[ METHOD_EXECUTED ]-- " + method  + "  -- [ METHOD_RESULT ]-- " + proceed.toString());
+        }catch (Exception e){
+            System.out.println("[ LOG_ERROR ]-- Method Logging Error when execution process" + e);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+
+        return proceed;
+    }
+}
